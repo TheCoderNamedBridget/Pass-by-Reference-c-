02c1e2af5204ce73bc962db2f5cf618277f9f6ca
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2020-09-17 09:05:39 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
+/*author : Bridget Naylor
+*/
+
 #include <stdio.h>
 #include <iostream>
 using namespace std;
@@ -25,25 +28,52 @@ int larger( int first, int second)
 
 int& largest(int& x, int& y, int& z)
 {
+    int num = 666;
+    int& error = num;
     
-    return ;
+    int *ptrX;//* = thing point to by 
+    ptrX = &x;//& = address of
+    int valueX;
+    valueX = *ptrX;//* = thing pointer to by
+    
+    int *ptrY;//* = thing point to by 
+    ptrY = &y;//& = address of
+    int valueY;
+    valueY = *ptrY;//* = thing pointer to by
+    
+    int *ptrZ;//* = thing point to by 
+    ptrZ = &z;//& = address of
+    int valueZ;
+    valueZ = *ptrZ;//* = thing pointer to by
+    
+    if ( larger(valueX, valueY) == valueY && larger(valueZ, valueY) == valueY)
+    {
+        return y;
+    }
+    else if ( larger(valueX, valueY) == valueX && larger(valueZ, valueX) == valueX )
+    {
+        return x;
+    }
+    else if ( larger(valueX, valueZ) == valueZ && larger(valueZ, valueY) == valueZ)
+    {
+        return z;
+    }
+    return error;//Error
+    
 }
 
 int main()
 {
-    int A = 2;
-    int B = 9;
-    int C = 11;
-    // int * aP = &A;
-    // int * bP = &B;
-    // int * cP = &C;
+    int A = 222;
+    int B = 1111;
+    int C = 0;
     
     int& a = A;
     int& b = B;
     int& c = C;
+
     
-    
-    cout<<larger( 2, 10 )<<endl;
+    cout<<larger( a, c )<<endl;
 
     cout<<largest( a, b, c )<<endl;
     
@@ -51,4 +81,5 @@ int main()
 
     return 0;
 }
+
 
